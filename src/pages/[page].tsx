e2c5f09384df280453a2e79fc6b9c3a988ada8a0
FerYoi <fer.rosel@hotmail.com>
2022-10-10 19:42:53 -0500

    fine

--- src/pages/[page].tsx
+++ src/pages/[page].tsx
@@ -15,7 +15,7 @@ type IPageUrl = {
 };
 
 const PaginatePosts = (props: IBlogGalleryProps) => (
-  <Main meta={<Meta title="Lorem ipsum" description="Lorem ipsum" />}>
+  <Main meta={<Meta title="StackStudent" description="StackBlog" />}>
     <BlogGallery posts={props.posts} pagination={props.pagination} />
   </Main>
 );
